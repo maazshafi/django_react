92c45980d569813d87a27b73ec4f318ce5ac17e8
Maaz Shafi <maaz.shafi@hotmail.com>
2022-01-27 22:42:09 -0500

    Refactored views and urls to manage easier

---
+++ backend/base/urls/order_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from base.views import order_views as views
+
+
+urlpatterns = [
+    # path("", views.getProducts, name="products"),
+    # path("<str:pk>/", views.getProduct, name="product"),
+]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,25 +1,17 @@
-from email import message
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework import serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .products import products
 
-
-from .models import Product, User
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.models import User
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
-
-# function based views b/c shows all logic within the view
-# classes are more expert level it seems
-# TODO Refactor to use classes
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -73,22 +65,3 @@ def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
-
-
-@api_view(["GET"])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def getWorld(request):
-    return "Hello"
