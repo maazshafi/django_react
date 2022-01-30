a0fa8c9124aeeb5cf4c6c8ee90c1988cfb7717cf
Maaz Shafi <maaz.shafi@hotmail.com>
2022-01-30 14:16:39 -0500

    user profile screen and getting user details

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -94,16 +94,15 @@ function RegisterScreen() {
         <Button type="submit" variant="primary">
           Register
         </Button>
-
-        <Row className="py-3">
-          <Col>
-            Have an Account?{' '}
-            <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
-              Sign In
-            </Link>
-          </Col>
-        </Row>
       </Form>
+      <Row className="py-3">
+        <Col>
+          Have an Account?{' '}
+          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+            Sign In
+          </Link>
+        </Col>
+      </Row>
     </FormContainer>
   );
 }
