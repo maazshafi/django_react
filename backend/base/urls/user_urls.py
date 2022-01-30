d67c3650f5f268c1665f648a730ff416a995445b
Maaz Shafi <maaz.shafi@hotmail.com>
2022-01-30 13:25:04 -0500

    add update profile endpoint

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("login/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("register/", views.registerUser, name="register"),
     path("profile/", views.getUserProfile, name="users-profle"),
+    path("profile/update/", views.updateUserProfile, name="user-profle-update"),
     path("", views.getUsers, name="users"),
 ]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -51,6 +51,26 @@ def registerUser(request):
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    # want to return back the new token
+    serializer = UserSerializerWithToken(user, many=False)
+
+    data = request.data
+    user.first_name = data["name"]
+    user.username = data["email"]
+    user.email = data["email"]
+
+    if data["password"] != "":
+        user.password = make_password(data["password"])
+
+    user.save()
+
+    return Response(serializer.data)
+
+
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
